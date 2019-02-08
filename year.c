7b96d1d25532193d8a5eda77b858d4caffa7797c
taravind712 <47447633+taravind712@users.noreply.github.com>
2019-02-08 19:08:08 +0530

    Create year.c

---
+++ year.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%4==0)
+    {
+    if(a%100==0)
+    {
+        if(a%400==0)
+        printf("yes");
+        else
+        printf("no");
+    }
+    else
+    printf("yes");
+    }
+    else
+    printf("no");
+}
