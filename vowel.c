28b82e9fe5bc96762ac96a61e75fd0b7492ae905
taravind712 <47447633+taravind712@users.noreply.github.com>
2019-02-08 18:22:51 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -7,7 +7,7 @@ void main()
 	scanf("%c",&ch);
 	if(ch=='a'|| ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' ||ch=='O' || ch=='u' || ch=='U')
 	printf("Vowel");
-	elseif((ch>'a' && ch<='z') ||  (ch>'A' && ch<='Z'))
+	else if((ch>'a' && ch<='z') ||  (ch>'A' && ch<='Z'))
 	    printf("Consonant");
 	else
 		printf("invalid");
